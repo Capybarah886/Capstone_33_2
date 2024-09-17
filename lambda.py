7c6001ad69dc39070067f2f1428d1010201ccf2a
Capybarah886 <ywan0632@uni.sydney.edu.au>
2024-09-18 02:13:25 +1000

    lamnda function uodated

--- lambda.py
+++ lambda.py
@@ -2,6 +2,8 @@ import base64
 import json
 import cv2
 import numpy as np
+import sys
+from skimage.filters import threshold_multiotsu
 
 class HandProcesser:
     def __init__(self, img):
@@ -16,13 +18,27 @@ class HandProcesser:
         (_, _), radius = cv2.minEnclosingCircle(contour)
         circle_area = np.pi * (radius ** 2)
         return abs(area - circle_area) / circle_area < epsilon
+    def detect_background(self, image):
+        # find the background region based on its greatest frequency
+        unique, counts = np.unique(image, return_counts=True)
+        for i in range(len(unique)):
+            print(f"Value: {unique[i]}, Frequency: {counts[i]}")
+        background_value = unique[np.argmax(counts)]
+        print(f"Background value: {background_value}")
+        masked_image = np.where(image == background_value, 0, 255)
+        return masked_image
 
     def get_contours(self):
         ycbcr = cv2.cvtColor(self.img, cv2.COLOR_BGR2YCrCb)
         _, cb, _ = cv2.split(ycbcr)
-        _, thresh1 = cv2.threshold(cb, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  
+        thresholds=threshold_multiotsu(cb, classes=3)
+        thresh1 = np.digitize(cb, bins=thresholds)
+        masked_image = self.detect_background(thresh1)
+        masked_image_8uc1 = masked_image.astype('uint8')
+
+        # _, thresh1 = cv2.threshold(cb, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  
         
-        CNTS, _ = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        CNTS, _ = cv2.findContours(masked_image_8uc1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if not CNTS:
             raise ValueError("No contours found in the image")
 
@@ -40,7 +56,7 @@ class HandProcesser:
     def get_size(self, hand, target_circle):
         if target_circle is None:
             raise ValueError("Target circle is None")
-        actual_area = 1.8*1.8 * 3.14  # radius = 1.8cm
+        actual_area = 1.6*1.6 * 3.14  # radius = 1.8cm
         pixel_ratio = actual_area / cv2.contourArea(target_circle)
         return cv2.contourArea(hand) * pixel_ratio
 
@@ -58,7 +74,6 @@ def b64_to_img(b64):
     return cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
 def lambda_handler(event, context):
-    # TODO implement
     img = event['body']
     img_b64=b64_to_img(img)
     try:
@@ -75,3 +90,18 @@ def lambda_handler(event, context):
             'statusCode': 400,
             'body': json.dumps('No data return')
         }
+if __name__ == "__main__":
+    img_path = "./images/image5.jpg"
+    img = cv2.imread(img_path)
+
+    # Create an instance of HandProcesser
+    hand = HandProcesser(img)
+
+    # Call the process method
+    result = hand.process()
+
+    # Assert that the result is not None
+
+    # Print the result
+    print(f"The hand area in the image is {result} squared centimeters")
+
